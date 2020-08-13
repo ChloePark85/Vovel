2985a6aee8d6566311a7ee76af943e9780a1c696
Hyunah Park <hapark85@gmail.com>
2020-08-13 20:30:38 +0900

    tested index.html

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def show_story():
 def show_chatify():
     return jsonify({'result': 'success', 'msg': '스토리를 변환했습니다.'})
 
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
 
     # name = request.args.get('name')
     # return render_template('detail.html', name = name)
