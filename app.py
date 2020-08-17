25be8e504915d3bd2780258c0aad6ae4a7775fae
Hyunah Park <hapark85@gmail.com>
2020-08-18 00:42:08 +0900

    edited init_db file

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ def home():
 
 @app.route('/api/show', methods= ['GET'])
 def show_story():
-    return jsonify({'result': 'success', 'msg': '스토리를 가져왔습니다.'})
+    story=list(db.myproject.find({}, {'_id':False}))
+    return jsonify({'result': 'success', 'storylines': story})
 
-@app.route('/api/transform', methods= ['GET'])
-def show_chatify():
-    return jsonify({'result': 'success', 'msg': '스토리를 변환했습니다.'})
+# @app.route('/api/transform', methods= ['GET'])
+# def show_chatify():
+#     return jsonify({'result': 'success', 'msg': '스토리를 변환했습니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- init_db.py
+++ init_db.py
@@ -5,34 +5,39 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db=client.dbsparta
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://ko.wikisource.org/wiki/%EA%B8%88%EB%8F%84%EB%81%BC', headers=headers)
-
-soup = BeautifulSoup(data.text, 'html.parser')
-
-story=soup.select("#mw-content-text > div.mw-parser-output > div > p")
 
 text=""
 for p_tag in story:
     text += p_tag.text
 
-print(text)
+# print(text)
 
 lines = text.split("\n")
 
-print(lines)
+# print(lines)
 
-for line in lines:
+def get_urls():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://ko.wikisource.org/wiki/%EA%B8%88%EB%8F%84%EB%81%BC', headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+    story = soup.select("#mw-content-text > div.mw-parser-output > div > p")
+
+
+for index, line in enumerate(lines):
     if line.startswith('“'):
+        line_type = "chat"
         print (f"Chat: {line}")
     else:
+        line_type = "narration"
         print (f"Narration: {line}")
 
-#     doc ={
-#         'narration':narration,
-#         'chat':chat
-#     }
-#
-#
-# db.myproject.insert_one(doc)
+    doc ={
+        'order': index,
+        'type': line_type,
+        'text': line,
+    }
+
+
+db.myproject.insert_one(doc)
